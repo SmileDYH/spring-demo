efbcbc104038ecde196cc196d24d5977536827d4
dyh <574916501@qq.com>
2022-11-25 17:41:01 +0800

    feat():springboot配置文件加载顺序及应用顺序测试（bootstrap、application[.properties .yml]）,引入springcloud依赖

--- src/main/java/com/dyh/springDemo/controller/DemoController.java
+++ src/main/java/com/dyh/springDemo/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.dyh.springDemo.controller;
 
 import com.dyh.springDemo.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,18 @@ public class DemoController {
     @Autowired
     private DemoService demoService;
 
+    @Value("${name}")
+    private String name;
+
+    @Value("${spring.application.name}")
+    private String aplicationName;
+
     @GetMapping("/test")
     public void test(){
         System.out.println("/demo/test");
         demoService.test();
+
+        System.out.println("name--->" + name);
+        System.out.println("aplicationName--->" + aplicationName);
     }
 }
